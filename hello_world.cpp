c4901d98dfb1ad8107c88bea2af8ca5f45fc387c
LiWinDom <wadim.gorbachev@mail.ru>
2024-04-11 13:39:23 +0300

    Implemented "Hello world" program

---
+++ hello_world.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <bitset>
+#include <set>
+#include <map>
+#include <deque>
+#include <iostream>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+int main() {
+std::cout << "Hello";
+cout << ", world!"; }
