04b1027dcaa216fe76163cce4e42c2c001851e7c
LiWinDom <wadim.gorbachev@mail.ru>
2024-04-11 14:00:59 +0300

    Added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,13 @@
+// "Including libraries"
 #include <string>
 #include <iostream>
 
 int main() {
+  // Asing name
   std::cout << "What is your name? ";
   std::string name;
   std::cin >> name;
+
+  // Printing result
   std::cout << "Hello world from " << name;
 }
