fc3c67b5083aef9c968a4524433d605af3dc872b
LiWinDom <wadim.gorbachev@mail.ru>
2024-04-11 13:51:41 +0300

    Improved codestyle

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,19 +1,9 @@
-#include <algorithm>
-#include <queue>
 #include <string>
-#include <bitset>
-#include <set>
-#include <map>
-#include <deque>
 #include <iostream>
-#include <vector>
-#include <limits>
-
-using namespace std;
 
 int main() {
-std::cout << "What is your name? ";
-std::string name;
-std::cin >> name;
-std::cout << "Hello world from ";
-cout << name; }
+  std::cout << "What is your name? ";
+  std::string name;
+  std::cin >> name;
+  std::cout << "Hello world from " << name;
+}
